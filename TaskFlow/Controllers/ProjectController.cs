3a3c62bbf4f54c2501a8f5c0603196e6b68703e0
Amr Rummaneh <amr.rummaneh2@gmail.com>
2026-07-01 15:43:30 +0300

    Add ProjectController with Index action

---
+++ TaskFlow/Controllers/ProjectController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskFlow.DataAccess.Repository;
+
+namespace TaskFlow.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProjectController(IUnitOfWork unitOfWork)
+        { 
+            _unitOfWork = unitOfWork; 
+        }
+        public IActionResult Index()
+        {
+            var projects = _unitOfWork.Project.GetAll();
+            return View(projects);
+        }
+    }
+}

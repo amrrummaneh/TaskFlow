6a5c59dd0f22b80c0f087ed96633f57846e4f83a
Amr Rummaneh <amr.rummaneh2@gmail.com>
2026-07-01 14:31:05 +0300

    Add generic Repository with base CRUD operations

---
+++ TaskFlow.DataAccess/Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFlow.DataAccess.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> predicate);
+        void Add(T entity);
+        void Remove(T entity);
+    }
+}
---
+++ TaskFlow.DataAccess/Repository/Repository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TaskFlow.DataAccess.Data;
+
+namespace TaskFlow.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return dbSet.ToList();
+        }
+        public T Get(Expression<Func<T, bool>> predicate)
+        {
+            return dbSet.Where(predicate).FirstOrDefault();
+        }
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+    }
+}

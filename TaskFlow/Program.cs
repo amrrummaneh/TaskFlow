2758af73b7377d680499394a321d9ae37c4ce0ef
Amr Rummaneh <amr.rummaneh2@gmail.com>
2026-07-01 14:42:25 +0300

    Register UnitOfWork in DI container

--- TaskFlow/Program.cs
+++ TaskFlow/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using TaskFlow.DataAccess.Data;
+using TaskFlow.DataAccess.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

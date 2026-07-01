ce1507786864242978bc3e7b8da5d5f942a139e2
Amr Rummaneh <amr.rummaneh2@gmail.com>
2026-07-01 13:39:19 +0300

    Add Project model

---
+++ TaskFlow.Models/Project.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFlow.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}

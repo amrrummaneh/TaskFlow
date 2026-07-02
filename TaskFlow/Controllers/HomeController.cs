3f23f24f237c3c16486583517e789713343425c1
Amr Rummaneh <amr.rummaneh2@gmail.com>
2026-07-02 14:38:12 +0300

    Complete Project CRUD with cards UI and cleanup

--- TaskFlow/Controllers/HomeController.cs
+++ TaskFlow/Controllers/HomeController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using TaskFlow.Models;
 
-namespace TaskFlow.Controllers
+namespace TaskFlow.Web.Controllers
 {
     public class HomeController : Controller
     {

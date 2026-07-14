72657b7773a6c6d5e70278672ba83bf4c18b76fb
Amr Rummaneh <amr.rummaneh2@gmail.com>
2026-07-14 10:55:00 +0300

    Add ITaskItemRepository and TaskItemRepository

---
+++ TaskFlow.DataAccess/Repository/TaskItemRepository.cs
@@ -0,0 +1,15 @@
+﻿using TaskFlow.DataAccess.Data;
+using TaskFlow.Models;
+
+namespace TaskFlow.DataAccess.Repository
+{
+    public class TaskItemRepository : Repository<TaskItem>, ITaskItemRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public TaskItemRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
